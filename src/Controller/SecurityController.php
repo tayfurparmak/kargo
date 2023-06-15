7faf5fe304c6b3e3c37c5502e0e8af78591c341b
Tayfur PARMAK <tayfur-parmak-1907@hotmail.com>
2023-06-15 18:24:24 +0300

    Register ve login yapıldı.

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class SecurityController extends AbstractController
+{
+    
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout()
+    {
+        return new RedirectResponse($this->urlGenerator->generate('app_homepage'));
+    }
+}
